014b6dbea1ba85168213bd8697efa8dd4225db00
ahsaan-habib <ahsan.habib.pro@gmail.com>
2021-01-10 14:41:04 +0600

    week 02 practice done

--- learningAngular/src/app/pages/landing/landing.component.ts
+++ learningAngular/src/app/pages/landing/landing.component.ts
@@ -8,13 +8,30 @@ import { Card } from 'src/app/shared/interfaces';
 })
 export class LandingComponent implements OnInit {
 
-  public htmlData : Card = {title: 'HTML-CSS' , count: 3, frequent: 'Weekly', body: 'We will learn HTML-CSS'};
-  public jsData: Card = {title: 'JavaScript' , count: 2, frequent: 'Weekly', body: 'We will learn js'};
-  public angularData: Card = {title: 'Angular' , count: 1, frequent: 'Monthly', body: 'We will learn angular. and more'};
+  public cardList : Card[] = [];
+
+  // public htmlData : Card = {title: 'HTML-CSS' , count: 3, frequent: 'Weekly', body: 'We will learn HTML-CSS'};
+  // public jsData: Card = {title: 'JavaScript' , count: 2, frequent: 'Weekly', body: 'We will learn js'};
+  // public angularData: Card = {title: 'Angular' , count: 1, frequent: 'Monthly', body: 'We will learn angular. and more'};
   
-  constructor() { }
+  constructor() { 
+
+    this.cardList = this.getCardList()
+
+  }
 
   ngOnInit(): void {
   }
+
+  private getCardList() :Card[]{
+    let allCard : Card[] = [];
+
+    allCard.push({title: 'HTML-CSS' , count: 3, frequent: 'Weekly', body: 'We will learn HTML-CSS'});
+    allCard.push({title: 'JavaScript' , count: 2, frequent: 'Weekly', body: 'We will learn js'});
+    allCard.push({title: 'Angular' , count: 1, frequent: 'Monthly', body: 'We will learn angular. and more'});
+    
+    return allCard;
+  }
+  
 
 }
