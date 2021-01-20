9efc3c72e4c963713a789d4da02158add71d6692
ahsaan-habib <ahsan.habib.pro@gmail.com>
2021-01-20 15:04:57 +0600

    initial commit

---
+++ Assignment03/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { DistrictComponent } from './pages/district/district.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+
+const routes: Routes = [
+
+  {path: 'landing' , component:LandingComponent},
+  {path: 'about' , component: AboutComponent},
+  {path: 'district', component: DistrictComponent},
+  {path: '', redirectTo: '/landing', pathMatch: 'full'},
+  {path: '**' , component: PageNotFoundComponent}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
